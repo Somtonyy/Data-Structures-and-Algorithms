ca807893b9d074ca1fcb5718f4e9e3dfc5b80c5f
Somtonyy <somtonyy@gmail.com>
2024-05-27 21:59:23 -0300

    TimeConversion

---
+++ HackerRank/TimeCoversion.java
@@ -0,0 +1,22 @@
+public class TimeCoversion {
+    public static String timeConversion(String s) {
+        // Write your code 
+            String hoursString = s.substring(0, 2);
+            int hours = Integer.parseInt(hoursString);
+            String result = s.substring(0,s.length() - 2);
+            if ( s.charAt(s.length() - 2) == 'P') {
+                if (hours >= 1 && hours <= 11) {
+                    hours = hours + 12;
+                    return (12 + Integer.parseInt(s.substring(0,2))) + result.substring(2);
+                }
+            }
+            else if (s.charAt(s.length() - 2) == 'A') {
+                if (hours == 12) {
+                    return 00 + result.substring(2);
+                }
+            }
+        return result;
+        }
+    
+    }
+}
