89e6bed91f6921ad99a5776aeff4ed8bac842270
Somtonyy <somtonyy@gmail.com>
2024-08-04 17:48:48 -0300

    Java Stack practice

---
+++ HackerRank/JavaStack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class JavaStack {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            System.out.println(isBalanced(sc.next()));
+        }
+        sc.close();
+    }
+
+    private static boolean isBalanced(String input) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } else if (stack.empty()) {
+                return false;
+            } else {
+                char top = stack.pop();
+                if ((top == '(' && ch != ')') || (top == '[' && ch != ']')
+                        || (top == '{' && ch != '}')) {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
